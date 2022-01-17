057f78cc0e33182d233a5f9159b972ecf32b8e7c
Bin Fu <75638688+binfu0728@users.noreply.github.com>
2022-01-17 18:02:11 +0000

    Add files via upload

---
+++ gdc/GDC_config.m
@@ -0,0 +1,28 @@
+% Change all related parameters here and save to configuration file
+s         = struct;
+% System parameters
+s.name    = '12_olig_c2';
+
+s.channel = 1;
+
+s.time    = 10;  %number of frame, time-scan
+s.zaxis   = 11;  %number of z-samples
+s.colour  = 3;   %number of colour channels
+s.imgLoad = 'multi-mean';
+
+s.upsampling        = 4;
+s.mode              = 'IF-olig';
+s.gaussian_size     = 20;
+s.bpass_size_l      = 0;
+s.bpass_size_h      = 0;
+s.bpass_order       = 0;
+
+s.intensity_precent = -1;%default -1
+s.area_precent      = -1;%default -1
+s.strelSize         = 5; %default 0
+%%
+clear
+load('config_14_olig_c3.mat');
+s.name = '14_olig_c3';
+s.channel = 3;
+save(['config_',s.name,'.mat'],'s')
