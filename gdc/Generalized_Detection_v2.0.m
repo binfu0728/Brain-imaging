daf04653a753e9c367cedee047485e9a49e9b5ca
Bin Fu <75638688+binfu0728@users.noreply.github.com>
2022-01-20 10:44:26 +0000

    Update Generalized_Detection_v2.0.m

--- gdc/Generalized_Detection_v2.0.m
+++ gdc/Generalized_Detection_v2.0.m
@@ -4,7 +4,7 @@
 clc;clear;addpath('util')
 filename          = '4neurites_2';
 
-load('./gdc_config/config_24_lb_c2.mat')
+load('./config_gdc/config_24_lb_c2.mat')
 img = loadImage(filename,s.imgLoad,s.time,s.zaxis,s.colour,s.channel);
 
 % Pre-processing
