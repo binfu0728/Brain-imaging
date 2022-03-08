31815584ebb45acb56eced93c52aa15e428a38fc
Bin Fu <75638688+binfu0728@users.noreply.github.com>
2022-03-08 21:48:07 +0000

    Update GDC_config.m

--- gdc/GDC_config.m
+++ gdc/GDC_config.m
@@ -2,18 +2,22 @@ clc;clear;addpath('util');
 % Change all related parameters here and save to configuration file
 s         = struct;
 % System parameters
-s.name    = '12_olig_c2';
+s.name    = '20_olig_c2';
 
 
 s.channel = 2;
 
-s.time    = 10;  %number of frame, time-scan
-s.zaxis   = 11;  %number of z-samples
+s.width   = 512;
+s.height  = 512;
+s.frames  = 10;  %number of frame, time-scan
+s.slices  = 11;  %number of z-samples
 s.colour  = 3;   %number of colour channels
+s.hyper   ='xyzct'; %from most frequenctly changing to least frequenctly changing
+
 s.imgLoad = 'mean';
 
 s.upsampling        = 4;
-s.mode              = 'IF-olig';
+s.mode              = 'IF-olig'; %IF,DAB,IF-olig
 s.gaussian_size     = 20;
 s.bpass_size_l      = 0;
 s.bpass_size_h      = 0;
