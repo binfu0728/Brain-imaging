a8b73d6d883749c08ce1fa79dec403d086a9de41
Bin Fu <75638688+binfu0728@users.noreply.github.com>
2022-03-08 21:56:51 +0000

    Update Generalized_Detection_v2_1.m

--- gdc/Generalized_Detection_v2_1.m
+++ gdc/Generalized_Detection_v2_1.m
@@ -1,32 +1,24 @@
 clc;clear;addpath('util')
-filename          = '5neurites_1.tif';
+filename = '5neurites_1.tif';
 
-%% lb/ln analysis
-s = loadJSON('config_16_lb_c1.json');
+% Load config
+s        = loadJSON('config_16_lb_c1.json');
 
-img = loadImage(filename,s);
-%img orginal image 512x512
+% Load image
+img      = loadImage(filename,s); %img: orginal image 512x512
+
 % Pre-processing
-[imgg,i]          = preFiltering(img,s);
-%imgg resampled image 2048x2048
-%i after-prefiltered image
+[img_resampled,img_processed] = preFiltering(img,s); %img_resampled: resampled image 2048x2048, img_processed: after-prefiltered image
 
 % Thresholding
-num_bins          = 2^16;
-counts            = imhist(i,num_bins);
-p                 = counts / sum(counts);
-omega             = cumsum(p);
+BW = thresholding(img_processed,s);
 
-idx               = find(omega>s.thres);
-t                 = (idx(1) - 1) / (num_bins - 1);
-BW                = imbinarize(i,t);
-BW                = imfill(BW,'holes');%binary mask
+% Post-processing
+BW = postFiltering(BW,img_resampled,s.intensity_precent,'intensity'); %dab use i, IF use imgg
+BW = postFiltering(BW,img_resampled,s.area_precent,'area');
+BW = postFiltering(BW,img_resampled,0,'structural_open',s.strelSize);
 
-% %% Post-processing
-BW                = postFiltering(BW,imgg,s.intensity_precent,'intensity');
-BW                = postFiltering(BW,imgg,s.area_precent,'area');
-BW                = postFiltering(BW,imgg,0,'structural_open',s.strelSize);
-
-f1 = figure; imshow(imgg,[]);
+% Plot
+f1 = figure; imshow(img_resampled,[]);
 plotBinaryMask(f1,BW,[0.6350 0.0780 0.1840]);
-plotScaleBar(f1,imgg,0.107/s.upsampling,5);
+plotScaleBar(f1,img_resampled,0.107/s.upsampling,5);
