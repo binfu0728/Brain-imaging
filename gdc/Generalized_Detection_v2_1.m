9ef987061619906e7b2f12e2fb4c1b7c05be59d4
Bin Fu <75638688+binfu0728@users.noreply.github.com>
2022-03-14 17:26:22 +0000

    Update Generalized_Detection_v2_1.m

--- gdc/Generalized_Detection_v2_1.m
+++ gdc/Generalized_Detection_v2_1.m
@@ -8,17 +8,17 @@ s        = loadJSON('config_16_lb_c1.json');
 img      = loadImage(filename,s); %img: orginal image 512x512
 
 % Pre-processing
-[img_resampled,img_processed] = preFiltering(img,s); %img_resampled: resampled image 2048x2048, img_processed: after-prefiltered image
+[img_upsampled,img_processed] = preFiltering(img,s); %img_resampled: resampled image 2048x2048, img_processed: after-prefiltered image
 
 % Thresholding
 BW = thresholding(img_processed,s);
 
 % Post-processing
-BW = postFiltering(BW,img_resampled,s.intensity_precent,'intensity'); %dab use i, IF use imgg
-BW = postFiltering(BW,img_resampled,s.area_precent,'area');
-BW = postFiltering(BW,img_resampled,0,'structural_open',s.strelSize);
+BW = postFiltering(BW,img_upsampled,s.intensity_precent,'intensity'); %dab use i, IF use imgg
+BW = postFiltering(BW,img_upsampled,s.area_precent,'area');
+BW = postFiltering(BW,img_upsampled,0,'structural_open',s.strelSize);
 
 % Plot
-f1 = figure; imshow(img_resampled,[]);
+f1 = figure; imshow(img_upsampled,[]);
 plotBinaryMask(f1,BW,[0.6350 0.0780 0.1840]);
-plotScaleBar(f1,img_resampled,0.107/s.upsampling,5);
+plotScaleBar(f1,img_upsampled,0.107/s.upsampling,5);
