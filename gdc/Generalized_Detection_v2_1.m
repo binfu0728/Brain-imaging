6d7110113e2e644ae775c7a01b7ebb61d7647611
Bin Fu <75638688+binfu0728@users.noreply.github.com>
2022-05-06 12:28:29 +0100

    Update Generalized_Detection_v2_1.m

--- gdc/Generalized_Detection_v2_1.m
+++ gdc/Generalized_Detection_v2_1.m
@@ -1,24 +1,25 @@
-clc;clear;addpath('util');addpath('config_gdc');
+clc;clear;addpath('lib')addpath('config_gdc');
 filename = '5neurites_1.tif';
 
 % Load config
-s        = loadJSON('config_16_lb_c1.json');
+s        = load.loadJSON('config_16_lb_c1.json');
 
 % Load image
-img      = loadImage(filename,s); %img: orginal image 512x512
+img      = load.loadImage(filename,s); %img: orginal image 512x512
 
 % Pre-processing
-[img_upsampled,img_processed] = preFiltering(img,s); %img_resampled: resampled image 2048x2048, img_processed: after-prefiltered image
+[img_upsampled,img_processed] = image.preFiltering(img,s); %img_resampled: resampled image 2048x2048, img_processed: after-prefiltered image
 
 % Thresholding
-BW = thresholding(img_processed,s);
+BW = image.thresholding(img_processed,s);
 
 % Post-processing
-BW = postFiltering(BW,img_upsampled,s.intensity_precent,'intensity'); %dab use i, IF use imgg
-BW = postFiltering(BW,img_upsampled,s.area_precent,'area');
-BW = postFiltering(BW,img_upsampled,0,'structural_open',s.strelSize);
+BW = image.postFiltering(BW,img_upsampled,s.intensity_precent,'intensity'); %dab use i, IF use imgg
+BW = image.postFiltering(BW,img_upsampled,s.area_precent,'area');
+BW = image.postFiltering(BW,img_upsampled,0,'structural_open',s.strelSize);
 
 % Plot
-f1 = figure; imshow(img_upsampled,[]);
-plotBinaryMask(f1,BW,[0.6350 0.0780 0.1840]);
-plotScaleBar(f1,img_upsampled,0.107/s.upsampling,5);
+f1 = figure;
+imshow(img_upsampled,[]);
+visual.plotBinaryMask(f1,BW,[0.6350 0.0780 0.1840]);
+visual.plotScaleBar(f1,img_upsampled,0.107/s.upsampling,5);
