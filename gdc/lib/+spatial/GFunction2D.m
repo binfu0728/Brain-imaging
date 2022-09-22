7849dfb05c4e21ae746b60356d0fbd94d0b038fd
Bin Fu <75638688+binfu0728@users.noreply.github.com>
2022-09-22 14:45:18 +0100

    Delete gdc directory

--- gdc/lib/+spatial/GFunction2D.m
+++
@@ -1,5 +0,0 @@
-function G_r = GFunction2D(data,box,r,dr)
-    [DIST,rbox,lambda] = boxboundary(data,box);
-    Nr  = meanpoints_g(r,DIST,rbox,dr);
-    G_r = Nr./(lambda*2*pi*dr.*r);
-end
--- gdc/lib/+spatial/meanpoints_g.m
+++
@@ -1,9 +0,0 @@
-function Nr = meanpoints_g(r,DIST,rbox,dr)
-    Nr = zeros(size(r)); %mean number of datapoints for each radius (mean number = density * area in CSR(complete spatial random))
-    for k = 1:length(r) %Edge correction method
-            I = find(rbox>r(k)); %numer of datapoints containing the full circle within the box
-        if ~isempty(I)
-            Nr(k) = sum(sum(DIST(2:end,I)<(r(k)+dr/2) & DIST(2:end,I)>(r(k)-dr/2)))/length(I);
-        end
-    end
-end
