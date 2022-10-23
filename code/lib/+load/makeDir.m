8e830f4df7535f252e2601a11fd2872331c6a74d
Bin Fu <bf341@cam.ac.uk>
2022-10-23 16:31:07 +0100

    local repo test

--- code/lib/+load/makeDir.m
+++
@@ -1,5 +0,0 @@
-function filepath = makeDir(filepath)
-    if ~exist(filepath, 'dir')
-        mkdir(filepath); 
-    end 
-end
--- code/lib/+spatial/LFunction2D.m
+++
@@ -1,4 +0,0 @@
-function L_r = LFunction2D(data,box,r)
-    K_r = KFunction(data,box,r);
-    L_r = sqrt(K_r/pi)-r;
-end
