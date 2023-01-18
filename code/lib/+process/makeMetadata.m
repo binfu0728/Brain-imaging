ce1ed488ac17812b45f811e895666de1c325d724
Bin Fu <bf341@cam.ac.uk>
2023-01-18 16:16:43 +0000

    fix metadata loading

--- code/lib/+process/makeMetadata.m
+++ code/lib/+process/makeMetadata.m
@@ -1,4 +1,4 @@
-function [T,foldername] = makeMetadata(filedir)
+function T = makeMetadata(filedir)
 % input  : fildir, the mian folder where the data is saved
 % 
 % output : T, the metadata table
@@ -11,19 +11,24 @@ function [T,foldername] = makeMetadata(filedir)
     filenames = fullfile(folders,names);
     num_slash = length(strfind(filedir,'\')); % how many slashes in the file direction
 
-    idx       = cell2mat(cellfun(@(x) strfind(x,'Round'),filenames,'UniformOutput',false));
-    rounds    = cellfun(@(x) x(idx:idx+5),filenames,'UniformOutput',false);
-    rsid      = cellfun(@(x) x(idx+5:idx+5),filenames,'UniformOutput',false); %assign rsid based on the round
+%     idx       = cell2mat(cellfun(@(x) strfind(x,'Round'),filenames,'UniformOutput',false));
+%     rounds    = cellfun(@(x) x(idx:idx+5),filenames,'UniformOutput',false);
+%     rsid      = cellfun(@(x) x(idx+5:idx+5),filenames,'UniformOutput',false); %assign rsid based on the round
     
+    rounds    = cell(length(filenames),1);
     samples   = cell(length(filenames),1);
-
+    rsid      = cell(length(filenames),1);
+  
     for i = 1:length(files)
         tmpt = filenames{i};
         idx  = strfind(tmpt,'\');
+        rounds{i}   = tmpt(idx(num_slash)+1:idx(num_slash+1)-1);
         samples{i}  = tmpt(idx(num_slash+1)+1:idx(num_slash+2)-1);
+        r           = regexp(rounds{i},'\d*','Match'); %extract the number in the round name
         s           = regexp(samples{i},'\d*','Match'); %extract the number in the sample name
+        r           = str2num(r{1});
         s           = str2num(s{1});
-        rsid{i}     = str2num(rsid{i}) + 0.01*s; %assign rsid based on the sample
+        rsid{i}     = r + 0.01*s; %assign rsid based on the sample
     end
     zi = num2cell(zeros(length(filenames),1));
     zf = zi;
