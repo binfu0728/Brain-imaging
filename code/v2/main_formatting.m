ea0e58294926c45811f830e1f708130bf2c76277
Bin Fu <75638688+binfu0728@users.noreply.github.com>
2023-02-02 14:20:38 +0000

    Update main_formatting.m

--- code/v2/main_formatting.m
+++ code/v2/main_formatting.m
@@ -1,36 +1,36 @@
-clc;clear;addpath(genpath('D:\code\'));
+clc;clear;addpath(genpath('D:\code\')); %path where you download the code
 
-s       = load.loadJSON('config_oligomer_sycamore.json');
-[filenames,filepath,z,rsid] = load.loadMeta('sycamore_compare_2_metadata.csv');
-prefix  = 'sycamore_compare_2_result\';
+% user input
+s       = load.loadJSON('config_oligomer_sycamore.json'); %config file used in aggregate detection (LB/oligomer both are fine)
+[filenames,filepath,z,rsid] = load.loadMeta('sycamore_compare_2_metadata.csv'); %metadata name for the current data
+prefix  = 'sycamore_compare_2_result\'; %result folder name
 
-%%
-nums_s   = [zeros(length(rsid),2),rsid];
-nums_z   = [];
-inten_s  = [zeros(length(rsid),1),rsid];
-inten_z  = [];
-inten_i  = {};
+%% turn seperate files into long format
+nums_z   = []; %number per slice
+inten_i  = {}; %intensity per oligomer 
 inten_t  = []; %tmpt holder
 
-for i = 1:108%length(rsid)
+for i = 1%:length(filenames)
 
-    [idx,filenames] = load.extractName([prefix,filepath{i}],{'large_aggregates_561','small_aggregates_561'});
+    [idx,filenames] = load.extractName([prefix,filepath{i}],{'large','small'}); %find correspounding results (specified in keywords {'large','small'}) in the folder
 
-    large = readmatrix(filenames{idx{1}}); %large
-    small = readmatrix(filenames{idx{2}});
+    large = readmatrix(filenames{idx{1}}); %large aggregate result
+    small = readmatrix(filenames{idx{2}}); %small aggregate result
 
-    lists = {small};
-    BWs   = {large};
-
-    [nums,inten,inten_all] = process.longFormatting(lists,BWs,z(i,:),rsid(i),s);
+    [nums,~,inten_all] = process.longFormatting({small},{large},z(i,:),rsid(i),s);
     nums_z          = [nums_z;nums];
-    nums_s(i,1:2)   = mean(nums(:,1:2),1);
-    inten_z         = [inten_z;inten];
-    inten_s(i,1)    = mean(inten(:,1),1);
     inten_t         = [inten_t;inten_all];
     i
 end
 
+%concat intensity into single cell
 for i = 1:size(inten_t,2)
     inten_i{i} = vertcat(inten_t{:,i});
 end
+
+%% write table
+T1 = array2table(nums_z,"VariableNames",{'small_nums_561','large_nums_561','rsid'});
+writetable(T1,'numbers_561_slice_.csv');
+
+T2 = array2table(inten_i{1},"VariableNames",{'sum_intensity','rsid'});
+writetable(T2,'intensity_561_oligomer.csv');
