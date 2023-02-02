e2c6139e1cf6c38e18fd7982834513e4e2876a51
Bin Fu <75638688+binfu0728@users.noreply.github.com>
2023-02-02 17:51:57 +0000

    Update loadMeta.m

--- code/lib/+load/loadMeta.m
+++ code/lib/+load/loadMeta.m
@@ -2,7 +2,8 @@ function [filenames,filepath,z,rsid,metadata] = loadMeta(fileDir)
     metadata           = readtable(fileDir,'VariableNamingRule','preserve');
     filenames          = metadata.filenames;
     [filepath,names,~] = fileparts(filenames);
-    filepath           = cellfun(@(x) load.extractPath(x,3),strcat(filepath,{'\'},names),'UniformOutput',false);
+    idx                = cell2mat(cellfun(@(x) strfind(x,'Round'),filepath,'UniformOutput',false));
+    filepath           = cellfun(@(x) x(idx:end),strcat(filepath,{'\'},names),'UniformOutput',false);
     z                  = [metadata.zi,metadata.zf];
     rsid               = metadata.rsid;
 end
