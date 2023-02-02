8527e162ac3c24940a380acc5b49834f8e34bd8d
Bin Fu <75638688+binfu0728@users.noreply.github.com>
2023-02-02 17:35:04 +0000

    Update main_aggregate.m

--- code/v2/main_aggregate.m
+++ code/v2/main_aggregate.m
@@ -1,17 +1,21 @@
-clc;clear;addpath(genpath('D:\code\'));
+clc;clear;addpath(genpath('C:\Users\Bigtree\Desktop\code\'));
+load('gain_sycamore.mat');
+load('offset_sycamore.mat');
+
 % for small and large aggregtes
-s1 = load.loadJSON('config_lb_biscut.json');
-s2 = load.loadJSON('config_oligomer_biscut.json');
-[filenames,filepath,z,rsid] = load.loadMeta('pilot_metadata.csv');
+s1 = load.loadJSON('config_lb_sycamore.json');
+s2 = load.loadJSON('config_oligomer_sycamore.json');
+s2.k2_dog  = 1.75;
+s2.percent = 0.975; 
+[filenames,filepath,z,rsid] = load.loadMeta('test_metadata.csv');
 
 %%
 saved = 0;
 
-for i = 1:length(filenames)
+for i = 1:10%:length(filenames)
     img        = load.Tifread(filenames{i});
-    img        = reshape(img,[s1.height,s1.width,s1.slices,s1.channel]);
-    img_c      = img(:,:,z(i,1):z(i,2),s1.channel);
-    [smallM,largeM,r_z,r_avg] = process.aggregateDetection(img_c,s1,s2,z(i,1),saved);
+    img_c      = double(img(:,:,z(i,1):z(i,2)));
+    [smallM,largeM,r_z] = process.aggregateDetection(img_c,s1,s2,saved,gain,offset);
 
     if saved == 1
         newFolder  = load.makeDir(fullfile(['.\pilot_result\',filepath{i}]));
@@ -20,13 +24,14 @@ for i = 1:length(filenames)
         writetable(boundaries,fullfile(newFolder,'result_large_aggregates_561.csv'));
     else
         for j = 1:size(img_c,3)
-            zimg = double(imresize(img_c(:,:,j),4));
-            figure;imshow(zimg,[]);
-            f    = visual.plotAll(zimg,largeM(:,:,j),[0.6350 0.0780 0.1840]);
-                   visual.plotBinaryMask(f,smallM(:,:,j),[0.8500 0.3250 0.0980])
+            zimg = uint16(img_c(:,:,j));
+            f1   = figure;imshow((zimg),[100 600]);
+            f    = figure;imshow((zimg),[100 600]);
+            visual.plotBinaryMask(f,largeM(:,:,j),[0.6350 0.0780 0.1840]);
+            visual.plotBinaryMask(f,smallM(:,:,j),[0.8500 0.3250 0.0980])
             pause(0.1);
         end
-        close all
+%         close all
     end
     i
 end
