56c0a316c518d7416aab3b27dad0bf106bbe69eb
Bin Fu <75638688+binfu0728@users.noreply.github.com>
2023-05-16 11:47:07 +0100

    Add files via upload

---
+++ code_v3/lib/+visual/plotAllMask.m
@@ -0,0 +1,20 @@
+function f = plotAllMask(img,smallM,largeM,flagS,flagL,contrast,pauseTime,range)
+    if nargin < 7
+        range = [1 size(img,3)];
+        pauseTime = 0.1;
+    elseif nargin < 8
+        range = [1 size(img,3)];
+    end
+    
+    for i = range(1):range(2)
+        f = figure; imshow(img(:,:,i),contrast);
+        pause(pauseTime);
+        if flagS == 1
+            visual.plotBinaryMask(f,smallM(:,:,i),[0.92,0.5,0.38]);
+        end
+        if flagL == 1
+            visual.plotBinaryMask(f,largeM(:,:,i),[0.23,0.5,0.45]);
+        end
+        pause(pauseTime);
+    end
+end
